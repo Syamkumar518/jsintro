6b99013e0705984668535414210c4d0fb7e3ab73
syamkumar <syamkumarks518@gmail.com>
2024-08-21 21:56:16 +0530

    initia

---
+++ script.js
@@ -0,0 +1,35 @@
+document.write("hello world");
+document.write("<br>");
+document.write("hello world");
+document.write("hello world");
+document.write("hello world");
+
+console.log("this is test massage");
+// console debug messge
+console.log("this is error");
+console.log("this is test massage");
+
+let person = "hello"
+
+var age
+age=10
+console.log(age)
+const pi=123
+console.log(pi)
+var a=10
+console.log(a )
+var b=20
+
+
+function varTest()
+{
+    var x=1;
+    if (true)
+    {
+        let y=2;
+        console.log(y);
+
+    }
+    console.log(y);
+}
+varTest
